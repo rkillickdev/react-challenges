3110446bcdcb8f686165e567c73f1fb7f8d63372
Rob Killick <robkillick@me.com>
2023-12-05 10:35:49 +0000

    add Content and PostItem components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import css from './App.module.css';
+import Content from './components/Content';
 import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
@@ -8,6 +9,7 @@ function App() {
     <div className={css.App}>
       < Sidebar />
       < NavBarForm />
+      <Content />
     </div>
   );
 }
